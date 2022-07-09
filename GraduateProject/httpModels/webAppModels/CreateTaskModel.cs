a5cb4a74532fe123ee5d6aa58fb51cc661c0d78c
Ali Elmorsy <asdw24111113@gmail.com>
2022-07-09 19:37:50 +0200

    fix validation of gender

--- GraduateProject/httpModels/webAppModels/CreateTaskModel.cs
+++ GraduateProject/httpModels/webAppModels/CreateTaskModel.cs
@@ -1,7 +1,10 @@
-﻿namespace GraduateProject.httpModels.webAppModels;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GraduateProject.httpModels.webAppModels;
 
 public class CreateTaskModel
 {
+    [Required]
     public byte Gender { get; set; }
     public IFormFile File { get; set; }
 }
